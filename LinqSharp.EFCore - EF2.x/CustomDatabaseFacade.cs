1aead0402c61728864d1289119ebd34c60e22814
zmjack <jack@zmland.com>
2020-05-26 17:07:17 +0800

    v3.0.9

---
+++ LinqSharp.EFCore - EF2.x/CustomDatabaseFacade.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinqSharp
+{
+    public class CustomDatabaseFacade : DatabaseFacade
+    {
+        public Action OnCommitted;
+        public Action OnRollbacked;
+        public Action OnDisposing;
+
+        public CustomDatabaseFacade(DbContext context) : base(context)
+        {
+        }
+
+        private IDbContextTransaction baseTransaction;
+
+        public override IDbContextTransaction BeginTransaction()
+        {
+            baseTransaction = base.BeginTransaction();
+            return new Transaction(this, baseTransaction.TransactionId);
+        }
+
+        public override void CommitTransaction()
+        {
+            base.CommitTransaction();
+            OnCommitted?.Invoke();
+        }
+
+        public override void RollbackTransaction()
+        {
+            base.RollbackTransaction();
+            OnRollbacked?.Invoke();
+        }
+
+        private void TransactionDisposing()
+        {
+            OnDisposing();
+        }
+
+        public class Transaction : IDbContextTransaction
+        {
+            private readonly CustomDatabaseFacade facade;
+
+            public Transaction(CustomDatabaseFacade customDatabaseFacade, Guid transactionId)
+            {
+                facade = customDatabaseFacade;
+                TransactionId = transactionId;
+            }
+
+            public Guid TransactionId { get; }
+
+            public void Commit() => facade.CommitTransaction();
+            public void Rollback() => facade.RollbackTransaction();
+            public void Dispose() => facade.TransactionDisposing();
+
+        }
+
+    }
+}
---
+++ LinqSharp.EFCore - EF3.1/CustomDatabaseFacade.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LinqSharp
+{
+    public class CustomDatabaseFacade : DatabaseFacade
+    {
+        public Action OnCommitted;
+        public Action OnRollbacked;
+        public Action OnDisposing;
+
+        public CustomDatabaseFacade(DbContext context) : base(context)
+        {
+        }
+
+        private IDbContextTransaction baseTransaction;
+
+        public override IDbContextTransaction BeginTransaction()
+        {
+            baseTransaction = base.BeginTransaction();
+            return new Transaction(this, baseTransaction.TransactionId);
+        }
+
+        public override void CommitTransaction()
+        {
+            base.CommitTransaction();
+            OnCommitted?.Invoke();
+        }
+
+        public override void RollbackTransaction()
+        {
+            base.RollbackTransaction();
+            OnRollbacked?.Invoke();
+        }
+
+        private void TransactionDisposing()
+        {
+            OnDisposing();
+        }
+
+        public class Transaction : IDbContextTransaction
+        {
+            private readonly CustomDatabaseFacade facade;
+
+            public Transaction(CustomDatabaseFacade customDatabaseFacade, Guid transactionId)
+            {
+                facade = customDatabaseFacade;
+                TransactionId = transactionId;
+            }
+
+            public Guid TransactionId { get; }
+
+            public void Commit() => facade.CommitTransaction();
+            public void Rollback() => facade.RollbackTransaction();
+            public void Dispose() => facade.TransactionDisposing();
+
+            public Task CommitAsync(CancellationToken cancellationToken = default) => Task.Run(() => facade.CommitTransaction());
+            public Task RollbackAsync(CancellationToken cancellationToken = default) => Task.Run(() => facade.RollbackTransaction());
+            public ValueTask DisposeAsync() => new ValueTask(Task.Run(() => facade.TransactionDisposing()));
+        }
+
+    }
+}
