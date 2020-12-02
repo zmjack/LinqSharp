875fc8e995bdf3f06c8b4c313b360b8f173336d3
zmjack <jack@zmland.com>
2020-12-02 11:41:49 +0800

    v3.1.6

--- push.cmd
+++ push.cmd
@@ -1,6 +1,6 @@
-nuget push "LinqSharp/bin/Release/LinqSharp.3.1.6.20.nupkg" -source nuget.org
-nuget push "LinqSharp.EFCore - EF2.x/bin/Release/LinqSharp.EFCore.2.1.35.nupkg" -source nuget.org
-nuget push "LinqSharp.EFCore - EF3.0/bin/Release/LinqSharp.EFCore.3.0.35.nupkg" -source nuget.org
-nuget push "LinqSharp.EFCore - EF3.1/bin/Release/LinqSharp.EFCore.3.1.35.nupkg" -source nuget.org
+nuget push "LinqSharp/bin/Release/LinqSharp.3.1.6.21.nupkg" -source nuget.org
+nuget push "LinqSharp.EFCore - EF2.x/bin/Release/LinqSharp.EFCore.2.1.35.1.nupkg" -source nuget.org
+nuget push "LinqSharp.EFCore - EF3.0/bin/Release/LinqSharp.EFCore.3.0.35.1.nupkg" -source nuget.org
+nuget push "LinqSharp.EFCore - EF3.1/bin/Release/LinqSharp.EFCore.3.1.35.1.nupkg" -source nuget.org
 nuget push "dotnet-orm/bin/Release/dotnet-orm.0.1.14.nupkg" -source nuget.org
 pause
