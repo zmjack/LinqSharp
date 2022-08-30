628ade79e8a57f2bd533efd8659714220e63d0db
Jack.Tan <195587293@qq.com>
2022-08-31 00:35:11 +0800

    v3.1.31

--- LinqSharp.EFCore/LinqSharp.EFCore/RowChangeInfo.cs
+++ LinqSharp.EFCore/LinqSharp.EFCore/RowChangeInfo.cs
@@ -3,7 +3,6 @@
 // you may not use this file except in compliance with the License.
 // See the LICENSE file in the project root for more information.
 
-using Castle.DynamicProxy.Contributors;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +11,8 @@ namespace LinqSharp.EFCore
 {
     public class RowChangeInfo : Dictionary<string, FieldChangeInfo>
     {
+        public bool IsValid { get; set; }
+
         public RowChangeInfo() { }
         public RowChangeInfo(IEnumerable<PropertyEntry> entries) : this(entries, false) { }
         public RowChangeInfo(IEnumerable<PropertyEntry> entries, bool modifiedOnly)
@@ -23,6 +24,7 @@ namespace LinqSharp.EFCore
 
             foreach (var entry in entries)
             {
+                IsValid = true;
                 Add(entry.Metadata.PropertyInfo.Name, new FieldChangeInfo
                 {
                     Display = DataAnnotationEx.GetDisplayName(entry.Metadata.PropertyInfo),
