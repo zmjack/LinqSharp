4a8284ce89a857e3340332783c7a3d127740c451
Jack.Tan <195587293@qq.com>
2022-09-07 14:44:11 +0800

    v3.1.33

--- push.cmd
+++ push.cmd
@@ -1,22 +1,22 @@
-nuget push "LinqSharp/bin/Release/LinqSharp.3.1.32.nupkg" -source nuget.org
+nuget push "LinqSharp/bin/Release/LinqSharp.3.1.33.nupkg" -source nuget.org
 
-nuget push "LinqSharp.EFCore/LinqSharp.EFCore - EF2.1/bin/Release/LinqSharp.EFCore.2.1.109.nupkg" -source nuget.org
-nuget push "LinqSharp.EFCore/LinqSharp.EFCore - EF3.0/bin/Release/LinqSharp.EFCore.3.0.109.nupkg" -source nuget.org
-nuget push "LinqSharp.EFCore/LinqSharp.EFCore - EF3.1/bin/Release/LinqSharp.EFCore.3.1.109.nupkg" -source nuget.org
-nuget push "LinqSharp.EFCore/LinqSharp.EFCore - EF5.0/bin/Release/LinqSharp.EFCore.5.0.9.nupkg" -source nuget.org
-nuget push "LinqSharp.EFCore/LinqSharp.EFCore - EF6.0/bin/Release/LinqSharp.EFCore.6.0.9.nupkg" -source nuget.org
+nuget push "LinqSharp.EFCore/LinqSharp.EFCore - EF2.1/bin/Release/LinqSharp.EFCore.2.1.110.nupkg" -source nuget.org
+nuget push "LinqSharp.EFCore/LinqSharp.EFCore - EF3.0/bin/Release/LinqSharp.EFCore.3.0.110.nupkg" -source nuget.org
+nuget push "LinqSharp.EFCore/LinqSharp.EFCore - EF3.1/bin/Release/LinqSharp.EFCore.3.1.110.nupkg" -source nuget.org
+nuget push "LinqSharp.EFCore/LinqSharp.EFCore - EF5.0/bin/Release/LinqSharp.EFCore.5.0.10.nupkg" -source nuget.org
+nuget push "LinqSharp.EFCore/LinqSharp.EFCore - EF6.0/bin/Release/LinqSharp.EFCore.6.0.10.nupkg" -source nuget.org
 
-nuget push "LinqSharp.EFCore.MySql/LinqSharp.EFCore.MySql - EF2.1/bin/Release/LinqSharp.EFCore.MySql.2.1.6.nupkg" -source nuget.org
-nuget push "LinqSharp.EFCore.MySql/LinqSharp.EFCore.MySql - EF3.0/bin/Release/LinqSharp.EFCore.MySql.3.0.6.nupkg" -source nuget.org
-nuget push "LinqSharp.EFCore.MySql/LinqSharp.EFCore.MySql - EF3.1/bin/Release/LinqSharp.EFCore.MySql.3.1.6.nupkg" -source nuget.org
-nuget push "LinqSharp.EFCore.MySql/LinqSharp.EFCore.MySql - EF5.0/bin/Release/LinqSharp.EFCore.MySql.5.0.0.nupkg" -source nuget.org
-nuget push "LinqSharp.EFCore.MySql/LinqSharp.EFCore.MySql - EF5.0/bin/Release/LinqSharp.EFCore.MySql.6.0.0.nupkg" -source nuget.org
+nuget push "LinqSharp.EFCore.MySql/LinqSharp.EFCore.MySql - EF2.1/bin/Release/LinqSharp.EFCore.MySql.2.1.7.nupkg" -source nuget.org
+nuget push "LinqSharp.EFCore.MySql/LinqSharp.EFCore.MySql - EF3.0/bin/Release/LinqSharp.EFCore.MySql.3.0.7.nupkg" -source nuget.org
+nuget push "LinqSharp.EFCore.MySql/LinqSharp.EFCore.MySql - EF3.1/bin/Release/LinqSharp.EFCore.MySql.3.1.7.nupkg" -source nuget.org
+nuget push "LinqSharp.EFCore.MySql/LinqSharp.EFCore.MySql - EF5.0/bin/Release/LinqSharp.EFCore.MySql.5.0.1.nupkg" -source nuget.org
+nuget push "LinqSharp.EFCore.MySql/LinqSharp.EFCore.MySql - EF6.0/bin/Release/LinqSharp.EFCore.MySql.6.0.1.nupkg" -source nuget.org
 
-nuget push "LinqSharp.EFCore.SqlServer/LinqSharp.EFCore.SqlServer - EF2.1/bin/Release/LinqSharp.EFCore.SqlServer.2.1.6.nupkg" -source nuget.org
-nuget push "LinqSharp.EFCore.SqlServer/LinqSharp.EFCore.SqlServer - EF3.0/bin/Release/LinqSharp.EFCore.SqlServer.3.0.6.nupkg" -source nuget.org
-nuget push "LinqSharp.EFCore.SqlServer/LinqSharp.EFCore.SqlServer - EF3.1/bin/Release/LinqSharp.EFCore.SqlServer.3.1.6.nupkg" -source nuget.org
-nuget push "LinqSharp.EFCore.SqlServer/LinqSharp.EFCore.SqlServer - EF5.0/bin/Release/LinqSharp.EFCore.SqlServer.5.0.0.nupkg" -source nuget.org
-nuget push "LinqSharp.EFCore.SqlServer/LinqSharp.EFCore.SqlServer - EF5.0/bin/Release/LinqSharp.EFCore.SqlServer.6.0.0.nupkg" -source nuget.org
+nuget push "LinqSharp.EFCore.SqlServer/LinqSharp.EFCore.SqlServer - EF2.1/bin/Release/LinqSharp.EFCore.SqlServer.2.1.7.nupkg" -source nuget.org
+nuget push "LinqSharp.EFCore.SqlServer/LinqSharp.EFCore.SqlServer - EF3.0/bin/Release/LinqSharp.EFCore.SqlServer.3.0.7.nupkg" -source nuget.org
+nuget push "LinqSharp.EFCore.SqlServer/LinqSharp.EFCore.SqlServer - EF3.1/bin/Release/LinqSharp.EFCore.SqlServer.3.1.7.nupkg" -source nuget.org
+nuget push "LinqSharp.EFCore.SqlServer/LinqSharp.EFCore.SqlServer - EF5.0/bin/Release/LinqSharp.EFCore.SqlServer.5.0.1.nupkg" -source nuget.org
+nuget push "LinqSharp.EFCore.SqlServer/LinqSharp.EFCore.SqlServer - EF6.0/bin/Release/LinqSharp.EFCore.SqlServer.6.0.1.nupkg" -source nuget.org
 
-nuget push "dotnet-orm/bin/Release/dotnet-orm.0.6.1.nupkg" -source nuget.org
+nuget push "dotnet-orm/bin/Release/dotnet-orm.0.6.2.nupkg" -source nuget.org
 pause
