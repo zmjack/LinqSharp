9043fa13e71dd554607f862191fa3b5db3a6a39c
zmjack <jack@zmland.com>
2023-09-14 00:46:56 +0800

    LinqSharp.7.0.18

--- push.cmd
+++ push.cmd
@@ -2,9 +2,9 @@ nuget push "LinqSharp/bin/Release/LinqSharp.7.0.18.nupkg" -source nuget.org
 
 nuget push "LinqSharp.EFCore/LinqSharp.EFCore.Abstractions/bin/Release/LinqSharp.EFCore.Abstractions.0.1.0.nupkg" -source nuget.org
 nuget push "LinqSharp.EFCore/LinqSharp.EFCore - EF2.1/bin/Release/LinqSharp.EFCore.2.1.7018.nupkg" -source nuget.org
-nuget push "LinqSharp.EFCore/LinqSharp.EFCore - EF3.1/bin/Release/LinqSharp.EFCore.3.1.7818.nupkg" -source nuget.org
-nuget push "LinqSharp.EFCore/LinqSharp.EFCore - EF5.0/bin/Release/LinqSharp.EFCore.5.0.7818.nupkg" -source nuget.org
-nuget push "LinqSharp.EFCore/LinqSharp.EFCore - EF6.0/bin/Release/LinqSharp.EFCore.6.0.7818.nupkg" -source nuget.org
+nuget push "LinqSharp.EFCore/LinqSharp.EFCore - EF3.1/bin/Release/LinqSharp.EFCore.3.1.7018.nupkg" -source nuget.org
+nuget push "LinqSharp.EFCore/LinqSharp.EFCore - EF5.0/bin/Release/LinqSharp.EFCore.5.0.7018.nupkg" -source nuget.org
+nuget push "LinqSharp.EFCore/LinqSharp.EFCore - EF6.0/bin/Release/LinqSharp.EFCore.6.0.7018.nupkg" -source nuget.org
 nuget push "LinqSharp.EFCore/LinqSharp.EFCore - EF7.0/bin/Release/LinqSharp.EFCore.7.0.18.nupkg" -source nuget.org
 
 nuget push "LinqSharp.EFCore.MySql/LinqSharp.EFCore.MySql/bin/Release/LinqSharp.EFCore.MySql.7.0.2.nupkg" -source nuget.org
